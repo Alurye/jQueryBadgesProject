5f7ce7ed87abaf2eab49e0772e36534082520feb
Arthur Lurye <arthurlurye@gmail.com>
2017-09-03 22:53:08 -0400

    finished

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
 $(function() {
 
   // your code will go here
+ $.ajax({
+ 		url:"https://www.codeschool.com/users/amoney92.json",
+ 		dataType: 'jsonp',
+ 		success: function(response){
+ 			console.log(response);
+ 			var completedCourses = response.courses.completed;
+ 				for (var i = 0; i < completedCourses.length; i++) {
+ 				
+ 					 $('#badges').append('<div class="course"><h3>' + completedCourses[i].title  + 
+ 					 	 '</h3>' + '<img src="' + completedCourses[i].badge +'">' + 
+ 					 	 '<a href="'+completedCourses[i].url + '" class="btn btn-primary">See Course</a>' + ' </div>');
+ 				};
+ 			console.log(completedCourses);
+ 			
+ 		}
 
+
+ });
 });
